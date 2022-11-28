6643a78ea924ad8b9d6af07f38038304879b192f
smbonilla <sierrabonilla14@gmail.com>
2022-11-28 16:18:06 +0000

    pytorch practice

---
+++ autograd.py
@@ -0,0 +1,13 @@
+import torch
+
+weights = torch.ones(4, requires_grad=True)
+
+for epoch in range(1):
+
+    model_output = (weights*3).sum()
+
+    model_output.backward() 
+
+    print(weights.grad)
+
+    weights.grad.zero_()
---
+++ pytorchTutorial.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+a = torch.ones(5)
+print(a)
+b = a.numpy()
+print(b)
+
+a.add_(1)
+print(a)
+print(b)
